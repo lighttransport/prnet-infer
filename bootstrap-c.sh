734eef063dfd3339a12ded694af6b08f9972fbcc
Syoyo Fujita <syoyo@lighttransport.com>
2019-04-16 15:47:22 +0900

    Update script.

--- bootstrap-c.sh
+++ bootstrap-c.sh
@@ -3,10 +3,10 @@
 rm -rf build
 
 # source code directory of tensorflow for C
-TF_C_DIR=/home/syoyo/local/tf-c
+TF_C_DIR=$HOME/local/tf-c
 
 cmake -DTENSORFLOW_C_DIR=${TF_C_DIR} \
-      -DSANITIZE_ADDRESS=On \
+      -DSANITIZE_ADDRESS=Off \
       -DWITH_DLIB=On \
       -DWITH_GUI=On \
       -Bbuild \
